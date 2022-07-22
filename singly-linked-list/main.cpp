b2510c6b28ede85484ebd6022e0e15c66c0fc754
meysam <mysm.shahbazi@gmail.com>
2022-07-22 07:42:47 -0400

    sll hw1

--- singly-linked-list/main.cpp
+++ singly-linked-list/main.cpp
@@ -5,13 +5,15 @@
 #include <vector>
 #include <algorithm>
 
+// #define DEBUG
+
 using namespace std;
 
 struct Node{
     int data;
     Node * next;
 
-    Node(int data):data(data){}
+    Node(int data):data(data){} 
 
 };
 
@@ -54,6 +56,7 @@ class LinkedList
 private:
     Node *head {nullptr};
     Node *tail {nullptr};
+#ifdef DEBUG    
     int length {0};
 
     vector<Node*> debug_data;	// add/remove nodes you use
@@ -68,12 +71,36 @@ private:
 		else
 			debug_data.erase(it);
 	}
+#endif
 public:
     LinkedList() {}
-    LinkedList(const LinkedList&) = delete;
+    // ~LinkedList()
+    // {
+    //     Node * cur = head;
+    //     Node * temp;
+    //     while(cur)
+    //     {
+    //         temp = cur->next;
+    //         delete cur;
+    //         cur = temp;
+    //     }     
+    // }
+    ~LinkedList()
+    {
+        Node * cur = head;
+        while (head)
+        {
+            cur = head->next;
+            delete head;
+            head = cur;
+        }
+        
+    }
+    LinkedList (const LinkedList& ) = delete;
     LinkedList &operator=(const LinkedList& another) = delete;
-
-    void debug_print_address() {
+#ifdef DEBUG
+    void debug_print_address() 
+    {
 		for (Node* cur = head; cur; cur = cur->next)
 			cout << cur << "," << cur->data << "\t";
 		cout << "\n";
@@ -107,7 +134,7 @@ public:
 		cout << "************\n"<<flush;
 	}
 
-    
+#endif
     void print()
     {
         Node * temp_head = head;
@@ -141,6 +168,39 @@ public:
             tail = node;
         }
     }
+    void insert_front(int value)
+    {
+        Node * new_head = new Node(value);
+        
+        if(head == nullptr)
+        {
+            head = new_head;
+            tail = new_head;
+        }
+        else 
+        {
+            new_head->next = head;
+            head = new_head;    
+        }
+    }
+    void delete_front()
+    {
+        if(head == nullptr)
+            return;
+
+        else if (tail == nullptr)
+        {
+            head = nullptr;
+            return;
+        }
+        else
+        {
+            Node * new_head = head->next;
+            delete head;
+            head = new_head;
+        }
+            
+    }
     Node * get_nth(int n)
     {
         int cnt = 0;
@@ -150,6 +210,25 @@ public:
         
         return nullptr;
     }
+    Node * get_nth_back(int n)
+    {
+        int cnt = 0;
+        int len = get_lenght();
+
+        // for(Node *cur = head; cur; cur = cur->next)
+        //     if(cnt++ == (len-n-1))
+        //         return cur;
+        return get_nth(len-n-1);
+    }
+    int get_lenght() const
+    {
+        int len = 0;
+        for(Node *cur = head; cur; cur = cur->next)
+            len++;
+            
+        return len;
+        
+    }
     int search(int value)
     {
         int cnt {0};
@@ -157,6 +236,18 @@ public:
             if(cur->data == value)
                 return cnt;
         return -1;
+    }
+    bool is_same(const LinkedList & another)
+    {
+        int alen = another.get_lenght();
+        if(this->get_lenght() != alen)
+            return false;
+
+        for(Node *cur=head,*acur=another.head;cur; cur= cur->next,acur=acur->next)
+            if (cur->data != acur->data)
+                return false;
+            
+        return true;
     }
     int improved_search(int value)
     {
@@ -233,6 +324,7 @@ public:
             }
         return -1;
     }
+#ifdef DEBUG
     void debug_verify_data_integrity()
     {
 		if (length == 0) 
@@ -270,7 +362,7 @@ public:
         }
         return oss.str();
     }
-
+#endif
 };
 int main()
 {
@@ -312,7 +404,41 @@ int main()
     list.print();
     cout<<list.improved_search2(1)<<endl;
     list.print();
+    list.insert_front(5);
+    list.print();
+    list.insert_front(6);
+    list.print();
+    list.delete_front();
+    list.print();
+    list.delete_front();
+    list.print();
+    LinkedList list2;
+    
+    cout<<list.get_nth_back(3)->data<<endl;
+    list2.insert_front(1);
+    list2.print();
+    list2.insert_front(2);
+    list2.print();
+    list2.insert_front(3);
+    list2.print();
+    list2.delete_front();
+    list2.print();
+    list2.delete_front();
+    list2.print();
+    list2.delete_front();
+    list2.print();
+    cout<<list2.is_same(list)<<endl;
+    LinkedList l1;
+    l1.insert_end(1);
+    l1.insert_end(2);
+    l1.insert_end(3);
+    LinkedList l2;
+    l2.insert_end(1);
+    l2.insert_end(2);
+    l2.insert_end(3);
+    cout<<l1.is_same(l2)<<endl;
+#ifdef DEBUG    
     cout<<list.debug_to_string()<<endl;
-
+#endif
     return 0;
 }
