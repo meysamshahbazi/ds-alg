dff35b2626cbd869c977abb7661681cebccd386b
meysamshahbazi <mysm.shahbazi@gmail.com>
2022-09-27 23:25:03 +0330

    create class for linked list

--- doubly-linked-list/main.cpp
+++ doubly-linked-list/main.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
+#include <cassert>
+#include <climits>
+
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
 
 struct Node
 {
     int data{};
     Node* next{};
     Node* prev{};
+
+    Node(int data): data(data) {}
+
+    void set(Node *next,Node * prev)
+    {
+        this->next = next;
+        this->prev = prev;
+    }
+    ~Node() {}
 };
 
+class LinkedList
+{
+private:
+    Node *head{};
+    Node * tail{};
+
+public:
+    LinkedList() {}
+    LinkedList(const LinkedList&) = delete;
+    LinkedList &operator=(const LinkedList &) = delete;
+
+};
 
 int main()
 {       
-    Node n;
+    
 
     return 0;
 }
