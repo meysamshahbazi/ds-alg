eb2bef7819b75793f6dbcf3297cd28349b694da3
meysamshahbazi <mysm.shahbazi@gmail.com>
2022-09-28 09:02:27 +0330

    insert back and front

--- doubly-linked-list/main.cpp
+++ doubly-linked-list/main.cpp
@@ -10,8 +10,8 @@ using namespace std;
 struct Node
 {
     int data{};
-    Node* next{};
-    Node* prev{};
+    Node* next{nullptr};
+    Node* prev{nullptr};
 
     Node(int data): data(data) {}
 
@@ -23,6 +23,13 @@ struct Node
     ~Node() {}
 };
 
+void link(Node* first, Node* second)
+{
+    if(first)
+        first->next = second;
+    if(second)
+        second->prev = first;
+}
 class LinkedList
 {
 private:
@@ -33,13 +40,43 @@ public:
     LinkedList() {}
     LinkedList(const LinkedList&) = delete;
     LinkedList &operator=(const LinkedList &) = delete;
-
+    void insert_end(int value)
+    {
+        Node *item = new Node(value);
+        if(!head)
+            head = tail= item;
+        else
+        {
+            link(tail,item);
+            tail = item;
+        }
+    }
+    void insert_front(int value)
+    {
+        Node *item = new Node(value);
+        if(!head)
+            head = tail= item;
+        else
+        {
+            link(item,head);
+            head = item;
+        }
+    }
+    void print()
+    {
+        for(Node *cur=head; cur; cur=cur->next)
+            cout<<cur->data<<" ";
+        cout<<endl;
+    }
 };
 
 int main()
 {       
-    
-
+    LinkedList l;
+    l.insert_end(1);l.insert_end(2);l.insert_end(3);
+    l.print(); 
+    l.insert_front(44);l.insert_front(55);l.insert_front(66);
+    l.print();
     return 0;
 }
 
