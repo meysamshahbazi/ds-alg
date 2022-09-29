0fa268017d5d8c915fe4ebb4a47f31e2c72c7aff
meysamshahbazi <mysm.shahbazi@gmail.com>
2022-09-29 11:34:08 +0330

    array based stack

--- stack/main.cpp
+++ stack/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cassert>
+
 using namespace std;
 
 class Stack
@@ -12,14 +14,63 @@ public:
     {
         array = new int [size];
     }
-    
+    ~Stack() 
+    {
+        delete [] array;
+    }
+    void push(int val)
+    {
+        assert(!is_full());
+        array[++top] = val;
+    }
+    int pop()
+    {
+        assert(!is_empty());
+        return array[top--];
+    }
+    int peek()
+    {
+        assert(!is_empty());
+        return array[top];
+    }
+    int is_full()
+    {
+        return top==size-1;
+    }
+    int is_empty()
+    {
+        return top ==-1;
+    }
+    void display()
+    {
+        for(int i=top;i>-1;i--)
+            cout<<array[i]<<" ";
+        cout<<endl;
+    }
+
 
 
 };
 
 int main() 
 {
+    Stack stk(3);
+	stk.push(10);
+	stk.push(20);
+	stk.push(30);
 
-    return;
+	if (!stk.is_full())
+		stk.push(50);
+	else
+		cout << "Full Stack\n";
+	stk.display();	// 30 20 10
+	cout << stk.peek() << "\n";	// 30
+
+	while (!stk.is_empty()) {
+		cout << stk.peek() << " ";
+		stk.pop();
+	} // 30 20 10
+    cout<<endl;
+    return 0;
 }
 
