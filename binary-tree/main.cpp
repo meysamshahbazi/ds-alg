c8b2f21525a4692436fc3b4048d7118d87b03e68
meysam <mysm.shahbazi@gmail.com>
2023-10-01 15:56:25 +0330

    print in order postorder

--- binary-tree/main.cpp
+++ binary-tree/main.cpp
@@ -6,27 +6,62 @@
 
 using namespace std;
 
+template<class T>
 struct Node
 {
-    int data{};
-    Node* left;
-    Node* right;
-    Node(int data): data{data} 
+    T data{};
+    Node<T>* left;
+    Node<T>* right;
+    Node(T data): data{data} 
     {
     }
 };
 
+void print_inorder(Node<char>* current)
+{
+    // cout<<current->left->data<<" ";
+    // cout<<current->data<<" ";
+    // cout<<current->right->data<<" ";
+    if(!current)
+        return;
+
+    print_inorder(current->left);
+    cout<<current->data<<" ";
+    print_inorder(current->right);
+}
+
+void print_postorder(Node<char> *current)
+{
+    if(!current)
+        return;
+    print_postorder(current->left);
+    print_postorder(current->right);
+    cout<<current->data<<" ";
+}
+
+void clear(Node<char>* current)
+{
+    if(!current)
+        return;
+
+    clear(current->left);
+    current->left = nullptr;
+    clear(current->right);
+    current->right = nullptr;
+    delete current;
+}
+
 int main()
 {
     // Create Nodes
-	Node* root = new Node(1);
-	Node* node2 = new Node(2);
-	Node* node3 = new Node(3);
-	Node* node4 = new Node(4);
-	Node* node5 = new Node(5);
-	Node* node6 = new Node(6);
-	Node* node7 = new Node(7);
-	Node* node8 = new Node(8);
+	Node<int>* root = new Node<int>(1);
+	Node<int>* node2 = new Node<int>(2);
+	Node<int>* node3 = new Node<int>(3);
+	Node<int>* node4 = new Node<int>(4);
+	Node<int>* node5 = new Node<int>(5);
+	Node<int>* node6 = new Node<int>(6);
+	Node<int>* node7 = new Node<int>(7);
+	Node<int>* node8 = new Node<int>(8);
 	// Link them!
 	root->left = node2;
 	root->right = node3;
@@ -42,7 +77,26 @@ int main()
 	cout << root->right->right->data << "\n";		// 6
 	cout << root->right->right->left->data << "\n";	// 8
 	cout << root->right->right->right << "\n";		// 0x00
-    
+    //  ----------------------------------------------------------
+    Node<char>* plus = new Node<char>('+');
+    Node<char>* n2 = new Node<char>('2');
+    Node<char>* n3 = new Node<char>('3');
+    plus->left = n2;
+    plus->right = n3;
+    print_inorder(plus);
+    cout<<"\n";
+
+    Node<char> *mul = new Node<char>('*');
+    Node<char>* n4 = new Node<char>('4');
+    mul->left = plus;
+    mul->right = n4;
+
+    print_postorder(mul);
+    cout<<"\n";
+    print_inorder(mul); cout<<"\n";
+    clear(mul);
+    cout<<"print for second time\n";
+    print_postorder(mul);cout<<"\n";
     return 0;
 }
 
