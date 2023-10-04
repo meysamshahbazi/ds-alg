8c7e6cc946c8793db4bcec48d4c6ccc8e46b93b9
meysamshahbazi <mysm.shahbazi@gmail.com>
2023-10-04 20:55:06 +0330

    add serach method

--- binary-search-tree/main.cpp
+++ binary-search-tree/main.cpp
@@ -8,6 +8,35 @@
 #include <utility>
 using namespace std;
 
+ class BinarySearchTree {
+private:
+    int data;
+    BinarySearchTree *left {};
+    BinarySearchTree *right{};
+public:    
+    BinarySearchTree() : data(0), left(nullptr), right(nullptr) {}
+    BinarySearchTree(int x) : data(x), left(nullptr), right(nullptr) {}
+    BinarySearchTree(int x, BinarySearchTree *left, BinarySearchTree *right) : data(x), left(left), right(right) {}
+
+    void print_inorder() 
+    {
+		if (left)
+			left->print_inorder();
+		cout << data << " ";
+		if (right)
+			right->print_inorder();
+	}
+    bool search(int target)
+    {
+        if (target == data)
+            return true;
+
+        if (target < data)
+            return left && left->search(target);
+
+        return right && right->search(target);
+    }
+ };
 
 int main()
 {
--- binary-tree/main.cpp
+++ binary-tree/main.cpp
@@ -215,16 +215,14 @@ public:
          if( !root1 && !root2)
             return true;
 
-        if(root1->data != root2->data)
+        if( (!root1 && root2) || (root1 && !root2))
             return false;
 
-        if( (!root1 && root2) || (root1 && !root2))
+        if(root1->data != root2->data)
             return false;
 
         return  ( flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right) ) ||
                 ( flipEquiv(root1->left,root2->right) && flipEquiv(root1->right,root2->left) );
-
-
     }
     string parenthesize_canonical()
     {
