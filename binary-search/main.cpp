8c3a0297bcdd90276891f3ab87a179dc7c36d76e
meysam <mysm.shahbazi@gmail.com>
2023-10-23 16:13:10 +0330

    hw

--- binary-search/main.cpp
+++ binary-search/main.cpp
@@ -87,20 +87,60 @@ vector<int> searchRange(vector<int>& nums, int target)
 vector<int> findRightInterval(vector<vector<int>>& intervals) 
 {
     int size = (int) intervals.size();
-    vector<int> starts(size);
-    vector<int> right_intervals(size);
-    
-    for (int i = 0; i < size; i++)
-        starts[i] = intervals[i][0];
 
-    for (int i = 0; i < size; i++) {
-        int key = starts[i];
-        starts[i] = intervals[i][1];
+    vector<int> right_intervals(size);
+
+    vector<pair<int,int>> starts_with_idx(size);
+    for (int i = 0; i < size; i++)
+        starts_with_idx[i] = make_pair(intervals[i][0], i);
+
+    sort(starts_with_idx.begin(), starts_with_idx.end());
+
+    vector<int> starts(size);
+    for (int i = 0; i < size; i++)
+        starts[i] = starts_with_idx[i].first;
+
+    for (int i = 0; i < size ; i++) {
+        int end_i = intervals[i][1];
+        auto it = lower_bound(starts.begin(), starts.end(), end_i);
         
+        if (it != starts.end())
+            right_intervals[i]  = starts_with_idx[it - starts.begin()].second;
+        else 
+            right_intervals[i] = -1;
     }
 
+    return right_intervals;
+}
+
+// https://leetcode.com/problems/valid-triangle-number/
+int triangleNumber(vector<int>& nums)
+{
+    sort(nums.begin(), nums.end());
+
+    vector<int> pos_nums;
+    for (auto p:nums)
+        if (p > 0)
+            pos_nums.push_back(p);
+
+    int total = 0;
+    
+    for (int i = 0; i < (int) pos_nums.size() - 2; i++) {
+        for (int j = i + 1; j < (int) pos_nums.size() - 1; j++) {
+            auto it = lower_bound(pos_nums.begin() + j + 1, pos_nums.end(), pos_nums[i] + pos_nums[j]);
+            int index =  it - pos_nums.begin() - j - 1;
+            total += index ;
+        }
+    }
+    return total;
+}
+
+// https://leetcode.com/problems/arranging-coins/
+int arrangeCoins(int n) 
+{
     
 }
+
 
 int main()
 {
@@ -125,7 +165,17 @@ int main()
     vector<int> hw1p1 = {5,7,7,8,8,10};
     vector<int> res_hw1p1  = searchRange(hw1p1, 6);
     print(res_hw1p1);
+    
+    // hw1 p2
+    vector<vector<int>> hw1p2 = {{3,4},{2,3},{1,2}};
+    hw1p2 = {{1,2}};
+    hw1p2 = {{1,4},{2,3},{3,4}};
+    auto hw1p2_res = findRightInterval(hw1p2);
+    print(hw1p2_res);
 
-
+    // hw1 p3
+    vector<int> hw1p3 = {2,2,3,4};
+    hw1p3 = {4,2,3,4};
+    cout<<triangleNumber(hw1p3)<<endl;
     return 0;
 }
