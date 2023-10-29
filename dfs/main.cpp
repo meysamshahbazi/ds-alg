4aeb5ed5b5a8ad444ec2aacdfa17804c0dba8a12
meysam <mysm.shahbazi@gmail.com>
2023-10-29 13:25:18 +0330

    hw 1

--- dfs/main.cpp
+++ dfs/main.cpp
@@ -8,6 +8,16 @@
 
 using namespace std;
 
+template<class T>
+void print(vector<T> &v)
+{
+    for (int i = 0; i < (int) v.size(); ++i) {
+		cout << v[i] << " ";
+	}
+    cout<<endl;
+}
+
+
 typedef vector<vector<int>> GRAPH;
 
 void add_directed_edge(GRAPH &graph, int from, int to) {
@@ -52,6 +62,87 @@ void reachability(GRAPH &graph) {
 	}
 }
 
+typedef unordered_map<int, vector<int>> GRAPHMP;
+
+void dfs(GRAPHMP &graph, int node, unordered_set<int> &visited, vector<int> &childs) {
+	visited.insert(node);
+	childs.push_back(node);
+	for (auto neighbour : graph[node]) {
+		if (visited.count(neighbour) == 0) {
+			dfs(graph, neighbour, visited, childs);
+		}
+	}
+}
+
+// https://leetcode.com/problems/kill-process/
+vector<int> killProcess(vector<int> &pid, vector<int> &ppid, int kill) {
+	int nodes = (int) pid.size();
+	GRAPHMP graph;
+
+	for (int i = 0; i < nodes; i++)
+		graph[ppid[i]].push_back(pid[i]);
+	
+	vector<int> childs;
+	unordered_set<int> visited;
+	dfs(graph, kill, visited, childs);
+	
+	return childs;
+}
+
+// https://leetcode.com/problems/employee-importance/
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+
+typedef unordered_map<int, pair<int, vector<int>> > GRAPHEM;
+
+int dfs(GRAPHEM &graph, int node)
+{
+	int res = graph[node].first;
+	for (auto neighbour: graph[node].second) {
+			res += dfs(graph, neighbour);
+	}
+	return res;
+}
+
+int getImportance(vector<Employee*> employees, int id) {
+	GRAPHEM graph;
+
+	for (auto &e: employees)
+		graph[e->id] = make_pair(e->importance, e->subordinates);
+	// No need for visited array in DAG, as no cycles :)
+	unordered_set<int> visited;
+	return dfs(graph, id);
+}
+
+
+void dfs2(GRAPH &graph, int node, vector<bool> &visited) {
+	visited[node] = true;
+	for(int neighbour : graph[node]) {
+		if (!visited[neighbour]) {
+			dfs2(graph, neighbour, visited);
+		}
+	}
+}
+
+// https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+int countComponents(int n, vector<vector<int>>& edges) {
+	GRAPH graph(n);
+	add_directed_edge(graph, edges);
+	vector<bool> visited(n);
+	int cc = 0;
+	for (int i = 0; i < n; ++i) {
+		if (!visited[i]) {
+			dfs2(graph, i, visited);
+			cc++;
+		}
+	}
+	return cc;
+}
+
 int main()
 {
 	vector<vector<int>> nodes_edge = { {2, 0}, {0 ,1}, {1 ,4}, {4 ,3}, {3 ,1}, {1 ,0}, {0 ,3}, {5 ,6}, {6 ,6} };
@@ -59,6 +150,17 @@ int main()
 	add_directed_edge(test_graph, nodes_edge);
 	print_adjaceny_matrix(test_graph);
 	reachability(test_graph);
+	// hw1 p1
+	vector<int> pid = {5, 2, 0, 1, 6, 3, 4};
+	vector<int> ppid = {-1, 5, 5, 5, 2, 2, 1};
+	vector<int> hw1p1 = killProcess(pid, ppid, 5);
+	print(hw1p1);
+	// hw1 p2 submited on leetcode!
+	// hw1 p3
+	vector<vector<int>> hw1p3 = {{0,1},{1,2},{3,4}};
+	cout<<countComponents(5,hw1p3)<<endl;
+	hw1p3 = {{0,1},{1,2},{3,4},{2,3}};
+	cout<<countComponents(5,hw1p3)<<endl;
     return 0;
 }
 
