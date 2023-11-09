e793f8c6d0214e1c7090e91cdf1112292dc7032d
meysamshahbazi <mysm.shahbazi@gmail.com>
2023-11-09 21:05:05 +0330

    hw 1

--- topo/main.cpp
+++ topo/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include<vector>
-#include<queue>
+#include <vector>
+#include <queue>
+#include <map>
 using namespace std;
 
+
+template<class T>
+void print(vector<T> &v)
+{
+    for (int i = 0; i < (int) v.size(); ++i) {
+		cout << v[i] << " ";
+	}
+    cout<<endl;
+}
+
+template<class T>
+void print(vector<vector<T>> &vec){
+	for(auto &v : vec)
+		print(v);
+}
 typedef vector<vector<int>> GRAPH;
 
 
@@ -59,6 +75,107 @@ vector<int> toposort(const GRAPH &adjlist)
     return ordering;
 }
 
+vector<int> lexicographic(const GRAPH &adjlist)
+{
+	int sz = adjlist.size();
+	vector<int> indegree(sz, 0);
+	for (int i = 0; i < sz; i++)
+		for(int j : adjlist[i])
+			indegree[j]++;
+
+	// map<int, int> ready;
+	priority_queue<int, std::vector<int>, std::greater<int> > ready;
+
+	for (int i = 0; i < sz; i++)
+		if (!indegree[i])
+			ready.push(i);
+	
+	vector<int> ordering;
+	while (!ready.empty()) {
+		int i = ready.top();
+		ready.pop();
+
+		ordering.push_back(i);
+
+		for (int j : adjlist[i])
+			if (--indegree[j] == 0)
+				ready.push(j);
+	}
+
+	if (ordering.size() != adjlist.size())
+		ordering.clear();
+
+	return ordering;
+}
+
+// https://leetcode.com/problems/parallel-courses/ premium
+int minimumSemesters(int n, vector<vector<int>>& relations)
+{
+	GRAPH adjlist(n);
+
+	for (auto edge: relations){
+		int to = edge[0] - 1;
+		int from = edge[1] - 1;
+		add_directed_edge(adjlist, from, to);
+	}
+
+	int sz = adjlist.size();
+    vector<int> indegree(sz, 0);
+    for (int i = 0; i < sz; i++)
+        for (int j : adjlist[i])
+            indegree[j]++;
+
+    queue<int> ready;
+
+    for (int i = 0; i < sz; i++)
+        if (!indegree[i])
+            ready.push(i);
+
+	int level = 0, found_nodes = 0;
+
+    while (!ready.empty()) {
+		int level_sz = ready.size();
+		
+		while ( level_sz--){
+			int i = ready.front();
+			ready.pop();
+			found_nodes++;
+
+			for (int j : adjlist[i])
+				if(--indegree[j] == 0)
+					ready.push(j);
+		}
+		level ++;
+    }
+
+    if (found_nodes != sz)
+        return -1;
+	
+	return level;
+}
+
+vector<GRAPH> getGraphs(const string &path)
+{
+	freopen(path.c_str(), "rt", stdin);
+	int cases;
+	cin >> cases;
+	vector<GRAPH> graphs;
+	while (cases--) {
+		int nodes, edges;
+		cin >> nodes >> edges;
+
+		GRAPH graph(nodes);
+
+		for (int e = 0; e < edges; ++e) {
+			int from, to;
+			cin >> from >> to;
+			add_directed_edge(graph, from, to);
+		}
+		graphs.push_back(graph);
+	}
+	return graphs;
+}
+
 int main() {
 	freopen("../data.txt", "rt", stdin);
 	int cases;
@@ -86,6 +203,19 @@ int main() {
 				cout << node << " ";
 			cout << "\n";
 		}
+	}
+
+	cout<<endl; 
+	cout<<endl; 
+
+	// hw1 p1 
+	vector<GRAPH> hw1p1 = getGraphs("../hw1p1.txt");
+	for (auto g : hw1p1) {
+		auto ordering = lexicographic(g);
+		if (ordering.empty())
+			cout << "There is a cycle\n";
+		else 
+			print(ordering);
 	}
 	return 0;
 }
