23dc42e99303721b91a988be8300e9296c180cab
meysamshahbazi <mysm.shahbazi@gmail.com>
2023-11-18 19:16:01 +0330

    add files

---
+++ dp-range/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <climits>
+#include <bits/stdc++.h> 
+#include <unordered_map>
+
+using namespace std;
+
+// https://leetcode.com/problems/longest-palindromic-substring/
+class Solution5 {
+    // 1 <= s.length <= 1000
+    
+public:
+    string longestPalindrome(string s) {
+        
+    }
+};
+
+
+
+int main()
+{
+
+    return 0;
+}
