0d8e117534521868670750027998616593dbee79
meysam <mysm.shahbazi@gmail.com>
2023-11-20 11:12:07 +0330

    longest palindromic substring

--- dp-range/main.cpp
+++ dp-range/main.cpp
@@ -9,41 +9,30 @@ using namespace std;
 
 // https://leetcode.com/problems/longest-palindromic-substring/
 class Solution5 {
-    // 1 <= s.length <= 1000
     string str;
     char mem[1001][1001];
 public:
-    int dp(int start, int end) {
-        if (start >= end)
-            return 1;
-        
-        auto &ret = mem[start][end];
+    int dp(int start, int len) {
+        auto &ret = mem[start][len];
         if (ret != -1)
             return ret;
-        
-        if (str[start] == str[end]) {
-            return dp(start + 1, end -1);
+
+        if (len <= 1)
+            return ret = 1;
+
+        if (str[start] == str[start + len - 1]) {
+            return ret = dp(start + 1, len - 2);
         }
-        return 0;
+        return ret = 0;
     }
     string longestPalindrome(string s) {
         memset(mem, -1, sizeof(mem));
-        str = s;
-        // for(int i = 0; i < (int) str.size(); i++) 
-        //     for(int j = (int) str.size() - 1; j >=0 ; j--) 
-        int start = 0;
-        int end = (int) str.size() - 1;
-        while (end >= start) {
-            if (dp(start, end) == 1)
-                return str.substr(start, end + 1);
-            if (dp(start, end - 1) == 1)
-                return str.substr(start, end);
-            if (dp(start + 1, end) == 1)
-                return str.substr(start + 1, end + 1);
+        str = s; 
+        for (int len = (int) str.size() ; len >= 0; len--)
+            for (int start = 0; start <= str.size() - len ; start++)
+                if (dp(start, len) == 1)
+                    return str.substr(start, len);
 
-            start++;
-            end--;
-        }
         return "";
     }
 };
@@ -53,7 +42,11 @@ public:
 int main()
 {
     Solution5 s5;
-    cout<<s5.longestPalindrome("babad")<<endl;
+    cout<<s5.longestPalindrome("0123babad")<<endl;
     cout<<s5.longestPalindrome("cbbd")<<endl;
+    cout<<s5.longestPalindrome("a")<<endl;
+    cout<<s5.longestPalindrome("ac")<<endl;
+    string str = "0123456789";
+    // cout<<str.substr(0,4);
     return 0;
 }
