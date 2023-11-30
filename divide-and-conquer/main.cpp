d7fb0c8660c32cfa8bb80c4ea806cc284f74a029
meysamshahbazi <mysm.shahbazi@gmail.com>
2023-11-30 11:42:30 +0330

    Add files

---
+++ divide-and-conquer/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <climits>
+#include <bits/stdc++.h> 
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+template<class T>
+void print(vector<T> &v)
+{
+    for (int i = 0; i < (int) v.size(); ++i) {
+		cout << v[i] << " ";
+	}
+    cout<<endl;
+}
+
+template<class T>
+void print(vector<vector<T>> &vec){
+	for(auto &v : vec)
+		print(v);
+}
+
+int main()
+{
+    
+    return 0;
+}
