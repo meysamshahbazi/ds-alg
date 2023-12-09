e5185792beef3b82d6d3d2d143f7cc58696274be
meysam <mysm.shahbazi@gmail.com>
2023-12-09 11:32:17 +0330

    finish hws

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -24,10 +24,12 @@ public:
         delete[] arr;
         arr = nullptr;
     }
+
     int get_size()
     {
         return size;
     }
+
     int get(int idx)
     {
         assert(0 <= idx  && idx < size);
@@ -42,140 +44,117 @@ public:
 
     void print()
     {
-        for(int i=0; i<size; i++)
+        for(int i = 0; i < size; i++)
             cout<<arr[i]<<" ";
         cout<<endl;
     }
+
     int find(int value)
     {
-        for(int i=0; i<size; i++)
-            if(arr[i] == value)
+        for (int i = 0; i < size; i++)
+            if (arr[i] == value)
                 return i;
         
         return -1;// -1 is indicator for Not found 
     }
+
     int get_front()
     {
         return arr[0];
     }
+
     int get_back()
     {
         return arr[size-1];
     }
-    void push_back1(int value)
-    {
-        // my strange way! this is NOT efficent!
-        int * old_arr = new int[size] {};
-        for(int i=0; i<size; i++)
-            old_arr[i] = arr[i];
 
-        delete[] arr;
-        arr = new int[size+1] {};
+    void push_back1(int value) { // based on size! O(n^2) for n push_back!
+        int *arr2 = new int[size + 1];
+        for (int i = 0; i < size; i++)
+            arr2[i] = arr[i];
 
-        for(int i=0; i<size; i++)
-            arr[i] = old_arr[i];
+        arr2[size++] = value;
 
-        arr[size] = value;
-        delete[] old_arr;
-        size +=1;
+        swap(arr, arr2);
+
+        delete []arr2;
     }
-    void push_back2(int value)
-    {
-        int * new_arr = new int [size+1];
 
-        for(int i=0; i<size;i++)
-            new_arr[i] = arr[i];
-
-        new_arr[size] = value;
-        size ++;
-        // code for swaping ponters
-        int * temp_ptr = new_arr;
-        new_arr = arr;
-        arr = temp_ptr;
-        delete[] new_arr;
-    }
     void expand_capacity()
     {
         capacity *= 2;
-        int * new_arr = new int [capacity];
-        for(int i=0; i<size;i++)
+        int * new_arr = new int[capacity] { };
+        for (int i = 0; i < size; i++)
             new_arr[i] = arr[i];
 
-
-        // code for swaping ponters
-        int * temp_ptr = new_arr;
-        new_arr = arr;
-        arr = temp_ptr;
+        swap(new_arr, arr);
         delete[] new_arr;
     }
-    void push_back(int value)
+
+    void push_back(int value) // O(n lg n) for n push back
     {
-        if(size+1 == capacity) // recreate new arr
-        {
+        if (size == capacity) // recreate new arr
            expand_capacity(); 
-        }
+        
         arr[size++] = value;    
     }
+
     void insert(int idx, int value)
     {
         assert(0 <= idx  && idx < size);
-        if(size+1 == capacity) // recreate new arr
-        {
+        if (size == capacity) // recreate new arr
            expand_capacity(); 
-        }
 
-        for(int i=size-1; i>=idx;i--)
+        for(int i = size - 1; i >= idx; i--)
             arr[i+1] = arr[i];
+
         arr[idx] = value;
         size++;
     }
-    void right_rotate()
+
+    void right_rotate() // hw1 p1
     {
         int back = get_back();
-        for(int i=size-2; i>=0;i--)
-            arr[i+1] = arr[i];
+        for (int i = size - 2; i >= 0; i--)
+            arr[i + 1] = arr[i];
 
         arr[0] = back;
     }
-    void left_rotate()
+
+    void left_rotate() // hw1 p2
     {
         int front = get_back();
-        for(int i=1; i<size-2;i++)
-            arr[i-1] = arr[i];
+        for (int i = 1; i < size - 2; i++)
+            arr[i - 1] = arr[i];
 
-        arr[size-1] = front;
+        arr[size - 1] = front;
     }
-    void right_rotate(int times)
+
+    void right_rotate(int times) // hw1 p3
     {
-        int times_ = times%size;
+        int times_ = times % size;
         while (times--)
-        {
             right_rotate();
-        }
-        
-            
     }
-    int pop(int idx)
+
+    int pop(int idx) // hw1 p4
     {
         int pop_value = arr[idx];
+        for(int i = idx; i < size - 1; i++)
+            arr[i] = arr[i + 1];
 
-        
-        for(int i=idx; i<size-1;i++)
-            arr[i] = arr[i+1];
-
-        size --;
+        size--;
         return pop_value;
     }
-    int find_transpostion(int value)
-    {
-        for(int i=0; i <size;i++)
-            if(arr[i] == value)
-                {
-                    left_rotate();
-                    return i;
-                }
 
-        return -1;
+    int find_transpostion(int value) // hw1 p5
+    {
+        int idx = find(value);
+        
+        if (idx > 0)  
+            swap(arr[idx - 1], arr[idx]);
+
+        return idx;
     }
 
 };
@@ -205,5 +184,13 @@ int main(int argc, const char ** argv)
     cout<<"Pop value is: "<<p<<endl;
     cout<<v.find(5)<<" "<<v.find(11)<<endl;
     cout<<v.find(777)<<" "<<v.find(11)<<endl;
+    cout<<"-------------------"<<endl;
+    // hw1 p 5
+    v.print();
+    for (int i = 0; i < 10; i++)
+        cout << v.find_transpostion(9)<< ", ";
+    
+    cout<< "\n";
+    v.print();
     return 0;
 }
