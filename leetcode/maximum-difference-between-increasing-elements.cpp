6a9e9433e865eb0f1e8b7ea5d7a2533a936646cf
meysam <mysm.shahbazi@gmail.com>
2023-12-10 10:14:09 +0330

    Edit

--- leetcode/maximum-difference-between-increasing-elements.cpp
+++ leetcode/maximum-difference-between-increasing-elements.cpp
@@ -10,10 +10,26 @@ using namespace std;
 class Solution {
     
 public:
+    int maximumDifference1(vector<int>& nums) { // O(n^2)
+        int dif = -1;
+        for (int i = 0; i < (int) nums.size() - 1; i++)
+            for (int j = i + 1; j < nums.size(); j++)
+                if ( nums[j] > nums[i] && nums[j] - nums[i] > dif)
+                    dif = nums[j] - nums[i];
+        
+        return dif;
+    }
     int maximumDifference(vector<int>& nums) {
+        vector<pair<int,int>> nums_idx;
         
-        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++)
+            nums_idx.push_back({nums[i], i});
+
+        sort(nums_idx.begin(), nums_idx.end());
         
+        
+
+        return -1;
     }
 };
 
@@ -21,4 +37,9 @@ public:
 
 int main() {
 
+    Solution s;
+    vector<int> nums;
+    
+    nums = {7,1,5,4};
+    s.maximumDifference(nums);
 }
